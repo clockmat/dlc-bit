fa777d255209d0381d3b2b454a2a95b28781a19e
viperadnan-git <viperadnan@gmail.com>
2024-09-07 01:46:31 +0530

    translate non-english names

--- rssbox/hooks/ptx_hook.py
+++ rssbox/hooks/ptx_hook.py
@@ -1,5 +1,8 @@
 import logging
 
+from feedparser import FeedParserDict
+from translators import translate_text
+
 from rssbox.enum import DownloadStatus
 from rssbox.hooks.hook import Hook
 from rssbox.modules.download import Download
@@ -9,6 +12,11 @@ logger = logging.getLogger(__name__)
 
 
 class PTXHook(Hook):
+    def on_new_entry(self, entry: FeedParserDict) -> FeedParserDict | bool:
+        if entry.link.startswith("https://sukebei.nyaa.si/"):
+            entry["title"] = translate_text(entry.title)
+        return entry
+
     def on_sonicbit_download_not_found(
         self, sonicbit: SonicBit, download: Download
     ) -> bool:
